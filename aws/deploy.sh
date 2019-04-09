238ec83dc22228ca539a13ea0a09b6e6140be013
julianoalberto <julianoalberto@gmail.com>
2019-04-09 09:21:49 -0300

    Updated scripts to output load balancer URL after creation of stack

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+instanceKeyName=julianoalberto
+instanceType=t2.micro
+instanceImageId=ami-02bcbb802e03574ba
+availabilityZone=us-east-2b
+vpc=vpc-487b7720
+loadBalancerSubnets=subnet-5222163a,subnet-855a38ff
+stackName=planetStack
+region=us-east-2
+
 aws cloudformation deploy \
---region us-east-2 \
+--region $region \
 --template-file stack.yaml \
---stack-name planetStack \
-# --parameter-overrides \
-# InstanceKeyName=julianoalberto \
-# InstanceType=t2.micro \
-# InstanceImageId=ami-02bcbb802e03574ba \
-# AvailabilityZone=us-east-2b \
-# VPC=vpc-487b7720 \
-# LoadBalancerSubnets=subnet-5222163a,subnet-855a38ff
+--stack-name $stackName \
+--parameter-overrides \
+InstanceKeyName=$instanceKeyName \
+InstanceType=$instanceType \
+InstanceImageId=$instanceImageId \
+AvailabilityZone=$availabilityZone \
+VPC=$vpc \
+LoadBalancerSubnets=$loadBalancerSubnets && \
+echo "App URL: $(aws cloudformation describe-stacks --region=$region --stack-name $stackName --query 'Stacks[0].Outputs[?OutputKey==`LoadBalancerURL`].OutputValue' --output text)"

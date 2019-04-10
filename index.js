2414dbe2409c5758ea4286e59a8203810894a501
julianoalberto <julianoalberto@gmail.com>
2019-04-10 08:58:07 -0300

    Updated hello message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('hello-world!')
+    res.send('hello-world')
 })
 
 app.listen(port, () => {
